2725bac6d153057220187989fa2922694d2c0c5a
Ruud Peters <ruud_peters96@hotmail.com>
2017-09-06 16:35:46 +0200

    started with beaming script

--- Assets/Scripts/Weapons/Beam.cs
+++ Assets/Scripts/Weapons/Beam.cs
@@ -6,13 +6,34 @@ using UnityEngine;
 [System.Serializable]
 public class Beam : Weapon
 {
+    private List<Transform> _beamableObjects;
+
     private void Start()
     {
+        _beamableObjects = new List<Transform>();
         _rotateSpeed = 3;
     }
 
     public override void Fire()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Beamable")
+            return;
+
+        if (!_beamableObjects.Contains(other.transform))
+            _beamableObjects.Add(other.transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Beamable")
+            return;
+
+        if (_beamableObjects.Contains(other.transform))
+            _beamableObjects.Remove(other.transform);
     }
 }
